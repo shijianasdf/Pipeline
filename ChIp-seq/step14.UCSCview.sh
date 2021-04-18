e0151bc24a70c0bbb8e97e8aecc343220ae85f37
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2021-04-19 07:29:03 +0800

    Create step14.UCSCview.sh

---
+++ ChIp-seq/step14.UCSCview.sh
@@ -0,0 +1,9 @@
+#' add custom track to UCSC
+#' Ref:UCSC custom track:  http://genome-asia.ucsc.edu/goldenPath/help/customTrack.html
+#' UCSC track hub : http://genome-asia.ucsc.edu/goldenPath/help/hgTrackHubHelp.html
+#' UCSC hub track 定义文档 ：http://genome-asia.ucsc.edu/goldenPath/help/trackDb/trackDbHub.html
+#' https://blog.csdn.net/ailongyang/article/details/60959239
+#' 
+#' https://genome.ucsc.edu/cgi-bin/hgCustom
+browser position chr21:33,031,597-33,041,570
+track type=bigWig name="SRS1282560_V576_Dnase_I_Hypersensitiviy" description="A Graph of Data from shijian" color=255,255,255 visibility=2 bigDataUrl=ftp://shijian2015@210.46.80.170/pub6/Temp/sj/GSE77737/DNase-seq/GenomeBrowser/SRS1282560_V576_Dnase_I_Hypersensitiviy.bw
