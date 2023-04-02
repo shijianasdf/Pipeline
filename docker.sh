fd08aef587b6ebac149a767f2c8b28aef963d175
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2023-04-02 18:13:41 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# https://hub.docker.com/ 在该网址上面查询下载已有的容器 biofly i l m
 docker pull data2intelligence/data2intelligence-suite
 docker run -it data2intelligence/data2intelligence-suite /bin/bash
 
