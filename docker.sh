59508f580be55d4c2aa75804a6aefbbaf25cb2a7
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2023-04-02 20:26:37 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 # https://hub.docker.com/ 在该网址上面查询下载已有的容器 biofly i l m
-docker pull data2intelligence/data2intelligence-suite
-docker run -it data2intelligence/data2intelligence-suite /bin/bash
+#查找所有容器
+docker ps -a 
+#下载docker容器
+docker pull data2intelligence/data2intelligence-suite 
+#加载容器
+docker run -it data2intelligence/data2intelligence-suite /bin/bash  
+#加载容器并创建共享文件夹
+docker run -it -v /Users/biofly/testCytosig:/data2intelligence/containerDir data2intelligence/data2intelligence-suite /bin/bash 
+
 
 
 
