66d44bdfcca11f3dfa25e9873faa5d18c790eefe
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2025-06-30 15:54:12 +0800

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,2 +1,8 @@
 # nohup后台运行
 nohup bash /home/shijian/project/cuttag/cuttag.sh > cuttag.log 2>&1 &
+
+#服务器里的R
+conda activate epigenetics
+/home/shijian/miniconda3/envs/epigenetics/bin/R
+conda activate R
+/home/shijian/miniconda3/envs/R/bin/R
