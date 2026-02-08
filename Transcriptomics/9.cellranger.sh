1dcbed5c7d528032fe35d18e43f5ec7b89c2367e
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2026-02-08 14:52:25 +0800

    Refactor cellRanger function for clarity and structure

--- Transcriptomics/9.cellranger.sh
+++ Transcriptomics/9.cellranger.sh
@@ -16,52 +16,52 @@
 #' --transcriptome=/home/shijian/refData/refdata-gex-GRCh38-2024-A
 
 cellRanger <- function(outDir,outfilepath,inputDir,
-                       pattern=".fastq.gz$",createBam=c("true","false")[1],
-                       ref="/home/shijian/refData/refdata-gex-GRCh38-2024-A"){
-  #创建输出目录
-  # if(!file.exists(outDir)){
-  #   dir.create(outdir)
-  # }
-  
-  library(stringr)
-  
-  if(!is.null(inputDir)){
-    # outDir <- "D:"
-    # inputDir <- "G:/肾透明细胞癌 尹志豪/"
-    # pattern <- ".fastq.gz$" 
-    t.files <- list.files(inputDir, pattern = pattern, full.names = TRUE, recursive = TRUE)
-    sampleNames <- list.files(inputDir, pattern = pattern, full.names = F, recursive = TRUE)
-    sampleNames <- gsub(".*/", "", sampleNames)
-    sampleNames <- unique(stringr::str_split(sampleNames,"(?=_S[0-9]+_L[0-9]{3}_R[12]_[0-9]{3}\\.fastq\\.gz$)",simplify = T)[,1])
+                         pattern=".fastq.gz$",createBam=c("true","false")[1],
+                         ref="/home/shijian/refData/refdata-gex-GRCh38-2024-A"){
+    #创建输出目录
+    # if(!file.exists(outDir)){
+    #   dir.create(outdir)
+    # }
     
-    #创建每个样本具体的输出目录
-    if(is.null(outDir)){
-      stop("please input outDir")
-    }
+    library(stringr)
     
-    #inputDirs <- unique(dirname(t.files))
-    outDirs <- file.path(outDir,sampleNames)
-    prefix_command <- "cellranger count --id=mRNA"
-    commands <- c()
-    for(i in 1:length(outDirs)){
-      if(!file.exists(outDirs[i])){
-        dir.create(outDirs[i])
+    if(!is.null(inputDir)){
+      # outDir <- "D:"
+      # inputDir <- "G:/肾透明细胞癌 尹志豪/"
+      # pattern <- ".fastq.gz$" 
+      t.files <- list.files(inputDir, pattern = pattern, full.names = TRUE, recursive = TRUE)
+      sampleNames <- list.files(inputDir, pattern = pattern, full.names = F, recursive = TRUE)
+      sampleNames <- gsub(".*/", "", sampleNames)
+      sampleNames <- unique(stringr::str_split(sampleNames,"(?=_S[0-9]+_L[0-9]{3}_R[12]_[0-9]{3}\\.fastq\\.gz$)",simplify = T)[,1])
+      
+      #创建每个样本具体的输出目录
+      if(is.null(outDir)){
+        stop("please input outDir")
       }
-      t.command <- paste("cd",outDirs[i],sep=" ")
-      commands <- c(commands,t.command)
-      cellranger.command <- paste0(prefix_command, " --fastqs=", inputDir , " --sample=", 
-                                   sampleNames[i], " --create-bam=",createBam, " --transcriptome=", ref)
-      commands <- c(commands,cellranger.command)
+      
+      inputDirs <- unique(dirname(t.files))
+      outDirs <- file.path(outDir,sampleNames)
+      prefix_command <- "cellranger count --id=mRNA"
+      commands <- c()
+      for(i in 1:length(outDirs)){
+        if(!file.exists(outDirs[i])){
+          dir.create(outDirs[i],recursive = F)
+        }
+        t.command <- paste("cd",outDirs[i],sep=" ")
+        commands <- c(commands,t.command)
+        cellranger.command <- paste0(prefix_command, " --fastqs=", inputDirs[i] , " --sample=", 
+                                     sampleNames[i], " --create-bam=",createBam, " --transcriptome=", ref)
+        commands <- c(commands,cellranger.command)
+      }
+      
+    }else{
+      stop("please input inputDir!")
     }
     
-  }else{
-    stop("please input inputDir!")
-  }
-  
-  writeLines(commands,con=outfilepath)
-  return(commands)
-
-}
+    writeLines(commands,con=outfilepath)
+    return(commands)
+    
+  }    
 
 cellRanger(outDir="/data/shijian/project/scMutDB/exp_matrix/SRP239174",
            outfilepath="/data/shijian/project/NGSCommand/cellranger_gex.sh",
