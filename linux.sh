bb51be595c4e4eb8fcc37022bae1683b5baf2d7b
BioWalker <39074222+shijianasdf@users.noreply.github.com>
2026-04-13 10:53:35 +0800

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -11,12 +11,18 @@ conda activate R
 
 #查看linux版本
 hostnamectl
+#查看磁盘结构
+lsblk
 #查看磁盘空间大小
 df -h
+df -h /
 #清空临时文件夹
 sudo rm -rf /tmp/*   cme811811
-sudo journalctl --vacuum-time=3d
+#这个是清理系统日志
+sudo journalctl --vacuum-time=3d  
 #清空日志文件
 du -sh /var/log
 rm -rf /var/log/*.log
 
+#已删除但仍占空间
+lsof | grep deleted
